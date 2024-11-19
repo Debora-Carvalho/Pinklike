10ade5254386bb411ed5cb0bd155699071b766a3
Debora-Carvalho <deboravolc2@gmail.com>
2024-11-19 18:06:46 -0300

    iniciando configurações de rotas

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        descricao: "Imagem 1",
+        imagem: "https://placecats.com/millie/300/150",
+    },
+    {
+        id: 2,
+        descricao: "Imagem 2",
+        imagem: "https://placecats.com/millie/300/150",
+    },
+    {
+        id: 3,
+        descricao: "Imagem 3",
+        imagem: "https://placecats.com/millie/300/150",
+    }
+];
+
 const app =  express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor ativo na porta 3000!");
 });
 
-app.get("/", (req, res) => {
-    res.status(200).send("Boas-vindas");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
 });
+
+function buscarPostPorID(id) {
+    return posts.findIndex( (post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
+});
+
+//para nao precisar ficar reiniciando o servidor, comando node --watch server.js
+// posso consultar os códigos de comunicação/status htpp, pelo site http.cat
