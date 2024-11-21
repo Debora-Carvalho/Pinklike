c77831e72d9b18554b30979b60857fbfe44df812
Debora-Carvalho <deboravolc2@gmail.com>
2024-11-21 11:18:48 -0300

    conectar a API ao MongoDB

--- server.js
+++ server.js
@@ -1,22 +1,7 @@
 import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
 
-const posts = [
-    {
-        id: 1,
-        descricao: "Imagem 1",
-        imagem: "https://placecats.com/millie/300/150",
-    },
-    {
-        id: 2,
-        descricao: "Imagem 2",
-        imagem: "https://placecats.com/millie/300/150",
-    },
-    {
-        id: 3,
-        descricao: "Imagem 3",
-        imagem: "https://placecats.com/millie/300/150",
-    }
-];
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 const app =  express();
 app.use(express.json());
@@ -25,20 +10,27 @@ app.listen(3000, () => {
     console.log("Servidor ativo na porta 3000!");
 });
 
-app.get("/posts", (req, res) => {
+async function  getTodosPosts() {
+    const db = conexao.db("pinklike-database")
+    const colecao = db.collection("posts")
+    return colecao.find().toArray()
+};
+
+app.get("/posts", async (req, res) => {
+    const posts = await getTodosPosts()
     res.status(200).json(posts);
 });
 
-function buscarPostPorID(id) {
-    return posts.findIndex( (post) => {
-        return post.id === Number(id)
-    })
-}
+// function buscarPostPorID(id) {
+//     return posts.findIndex( (post) => {
+//         return post.id === Number(id)
+//     })
+// }
 
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorID(req.params.id)
-    res.status(200).json(posts[index]);
-});
+// app.get("/posts/:id", (req, res) => {
+//     const index = buscarPostPorID(req.params.id)
+//     res.status(200).json(posts[index]);
+// });
 
 //para nao precisar ficar reiniciando o servidor, comando node --watch server.js
 // posso consultar os códigos de comunicação/status htpp, pelo site http.cat
