23595167c708e969e2f3275ad97b8b2d42f6ad70
Debora-Carvalho <deboravolc2@gmail.com>
2024-11-21 22:34:28 -0300

    add arquivos

--- server.js
+++ server.js
@@ -1,36 +1,13 @@
 import express from "express";
-import conectarAoBanco from "./src/config/dbConfig.js";
+import routes from "./src/routes/postsRoutes.js";
 
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+const app = express();
 
-const app =  express();
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
-    console.log("Servidor ativo na porta 3000!");
+    console.log("Servidor escutando na porta 3000!");
 });
-
-async function  getTodosPosts() {
-    const db = conexao.db("pinklike-database")
-    const colecao = db.collection("posts")
-    return colecao.find().toArray()
-};
-
-app.get("/posts", async (req, res) => {
-    const posts = await getTodosPosts()
-    res.status(200).json(posts);
-});
-
-// function buscarPostPorID(id) {
-//     return posts.findIndex( (post) => {
-//         return post.id === Number(id)
-//     })
-// }
-
-// app.get("/posts/:id", (req, res) => {
-//     const index = buscarPostPorID(req.params.id)
-//     res.status(200).json(posts[index]);
-// });
 
 //para nao precisar ficar reiniciando o servidor, comando node --watch server.js
 // posso consultar os códigos de comunicação/status htpp, pelo site http.cat
