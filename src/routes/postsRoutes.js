937d748a6e7c1341e5e38afb42bc89f3a06cce4d
Debora-Carvalho <deboravolc2@gmail.com>
2024-11-22 20:07:21 -0300

    deploy do projeto completo

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,15 @@
 //importando as bibliotecas necessárias
 import express from "express";
 import multer from "multer";
+import cors from "cors";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSucessStatus: 200
+}
 
 //importando as funções
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -19,12 +25,17 @@ const upload = multer({ dest: "./uploads" , storage})
 const routes = (app) => {
     // permite que o servidor interprete requisições com o corpo no formato JSON
     app.use(express.json());
+
+    app.use(cors(corsOptions))
     // rota para buscar todos os posts
     app.get("/posts", listarPosts);
     // rota para criar um post
-    app.post("/posts", postarNovoPost)
-    app.post("/upload", upload.single("imagem"), uploadImagem)
-}
+    app.post("/posts", postarNovoPost);
+    // rota para upload de imagens (assumindo uma única imagem chamada "imagem")
+    app.post("/upload", upload.single("imagem"), uploadImagem);
+
+    app.put("/upload/:id", atualizarNovoPost);
+};
 
 export default routes;
 
