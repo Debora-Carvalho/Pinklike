ef977af2b3fece9f43f84501d0d9e257010d0574
Debora-Carvalho <deboravolc2@gmail.com>
2024-11-22 18:58:24 -0300

    implementando armazenamento e upload de imagens

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -2,8 +2,17 @@ import conectarAoBanco from "../config/dbConfig.js";
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-export async function getTodosPosts(){
+export async function getTodosPosts() {
+    //seleciona o banco de dados "pinklike-database"
+    const db = conexao.db("pinklike-database");
+    //seleciona a coleção "posts" dentro do banco de dados
+    const colecao = db.collection("posts");
+    //retorna um array com todos os documentos da coleção
+    return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
     const db = conexao.db("pinklike-database");
     const colecao = db.collection("posts");
-    return colecao.find().toArray();
+    return colecao.insertOne(novoPost)   
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,29 @@
+//importando as bibliotecas necessárias
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+
+//importando as funções
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
 
 const routes = (app) => {
+    // permite que o servidor interprete requisições com o corpo no formato JSON
     app.use(express.json());
-    
+    // rota para buscar todos os posts
     app.get("/posts", listarPosts);
+    // rota para criar um post
+    app.post("/posts", postarNovoPost)
+    app.post("/upload", upload.single("imagem"), uploadImagem)
 }
 
 export default routes;
